6468e7e3fe843a2c2fbddf884825fb698a0403b1
Aneesh Bhat <imaneeshbhat@gmail.com>
2022-02-01 23:23:55 +0530

    chore: design changes for add_screen (need to add linear gradient)

--- flutter_expense_tracker/lib/ui/screens/add_screen.dart
+++ flutter_expense_tracker/lib/ui/screens/add_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_expense_tracker/ui/utils/constants.dart';
 import 'package:flutter_expense_tracker/ui/widgets/squircle_widget.dart';
 
@@ -6,6 +7,8 @@ class AddScreen extends StatelessWidget {
   // const AddScreen({ Key? key }) : super(key: key);
 
   final scrollController = ScrollController();
+
+  final EdgeInsets childPadding = EdgeInsets.all(10);
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +33,14 @@ class AddScreen extends StatelessWidget {
               padding: SYMMETRIC_PADDING_HORIZONTAL,
               child: Column(
                 children: [
-                  createInputField("Category"),
-                  addVerticleSpace(PADDING_VALUE),
-                  createInputField("Note"),
-                  addVerticleSpace(PADDING_VALUE),
-                  createInputField("Date"),
-                  addVerticleSpace(PADDING_VALUE),
+                  createAmountField(),
+                  addVerticleSpace(40),
+                  createInputField("Category", Icons.star),
+                  addVerticleSpace(15),
+                  createInputField("Note", Icons.note),
+                  addVerticleSpace(15),
+                  createInputField("Date", Icons.date_range),
+                  addVerticleSpace(15),
                 ],
               ),
             ),
@@ -45,20 +50,64 @@ class AddScreen extends StatelessWidget {
     );
   }
 
-  Widget createInputField(String labelText) {
-    final topPadding = 10.0;
-    final bottomPadding = 25.0;
+  Widget createAmountField() {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      padding: childPadding,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(40.0),
+        ),
+      ),
+      child: Center(
+        child: TextField(
+          keyboardType: TextInputType.number,
+          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+          textAlign: TextAlign.center,
+          style: TextStyle(color: COLOR_BLUE_GREY, fontSize: 32),
+          decoration: InputDecoration(
+            isDense: true,
+            prefix: Text(
+              "\$",
+              textAlign: TextAlign.right,
+              textDirection: TextDirection.rtl,
+            ),
+            prefixStyle: TextStyle(
+              color: COLOR_BLUE_GREY,
+              fontWeight: FontWeight.w700,
+              fontSize: 18,
+            ),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
+  }
 
+  Widget createInputField(String text, IconData iconData) {
     return SquircleWidget(
       shadowElevation: 0.0,
-      childPadding: EdgeInsets.fromLTRB(
-        PADDING_VALUE,
-        topPadding,
-        PADDING_VALUE,
-        bottomPadding,
-      ),
-      child: TextField(
-        decoration: InputDecoration(labelText: labelText),
+      childPadding: childPadding,
+      child: createTextField(text, iconData),
+    );
+  }
+
+  Widget createTextField(String text, IconData? iconData) {
+    return TextField(
+      // textAlign: TextAlign.center,
+      style: TextStyle(color: COLOR_BLUE_GREY, fontSize: 18),
+      decoration: InputDecoration(
+        isDense: true,
+        hintText: text,
+        hintStyle: TextStyle(
+          color: COLOR_BLUE_GREY,
+          fontWeight: FontWeight.w700,
+          fontSize: 18,
+        ),
+        hintTextDirection: TextDirection.ltr,
+        icon: Icon(iconData),
+        border: InputBorder.none,
       ),
     );
   }
